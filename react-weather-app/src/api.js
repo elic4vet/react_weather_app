f028a94accdb8c434a922dbe60c20fcd32cacb1d
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-07-17 21:38:13 +0200

    created the main app

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5b701bb3camshff01aaad0e5edffp1788e3jsn293a1ac8ee85",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo ";

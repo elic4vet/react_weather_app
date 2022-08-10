2aaadc773a5c325f3ebe7ef06535200499a7df9f
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-08-10 19:09:05 +0200

    Update current-weather.js

--- react-weather-app/src/components/current-weather/current-weather.js
+++ react-weather-app/src/components/current-weather/current-weather.js
@@ -4,11 +4,42 @@ const CurrentWeather = () => {
   return (
     <div className="weather">
       <div className="top">
-        <p className="city"> Belgrade</p>
-        <p className="weather-description">Sunny </p>
-      </div>
+        <div>
+          <p className="city"> Belgrade</p>
+          <p className="weather-description">Sunny </p>
+        </div>
 
-      <img alt="weather" className="weather-icon" src="icons/01d.png" />
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
+
+        <div className="bottom">
+          <p className="temperature"> 18°C</p>
+          <div className="details">
+            <div className="parameter-row">
+              <span className="parameter-label">Details </span>
+            </div>
+
+            <div className="parameter-row">
+              <span className="parameter-label"> Feels like </span>
+              <span className="parameter-value"> 22°C </span>
+            </div>
+
+            <div className="parameter-row">
+              <span className="parameter-label"> Wind </span>
+              <span className="parameter-value"> 2m/s </span>
+            </div>
+
+            <div className="parameter-row">
+              <span className="parameter-label"> Humidity </span>
+              <span className="parameter-value"> 15% </span>
+            </div>
+
+            <div className="parameter-row">
+              <span className="parameter-label"> Pressure </span>
+              <span className="parameter-value"> 15 hPa </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

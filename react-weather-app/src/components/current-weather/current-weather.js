ce993a98ffcb9517b24ae2de76c1b17452dcd0df
elic4vet <86532060+elic4vet@users.noreply.github.com>
2022-08-12 12:49:48 +0200

    Update current-weather.js

--- react-weather-app/src/components/current-weather/current-weather.js
+++ react-weather-app/src/components/current-weather/current-weather.js
@@ -30,17 +30,17 @@ const CurrentWeather = ({ data }) => {
 
           <div className="parameter-row">
             <span className="parameter-label"> Wind </span>
-            <span className="parameter-value"> 2m/s </span>
+            <span className="parameter-value">{data.wind.speed} m/s </span>
           </div>
 
           <div className="parameter-row">
             <span className="parameter-label"> Humidity </span>
-            <span className="parameter-value"> 15% </span>
+            <span className="parameter-value"> {data.main.humidity} % </span>
           </div>
 
           <div className="parameter-row">
             <span className="parameter-label"> Pressure </span>
-            <span className="parameter-value"> 15 hPa </span>
+            <span className="parameter-value">{data.main.pressure} hPa </span>
           </div>
         </div>
       </div>
